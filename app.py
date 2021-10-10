294a79516bd077374d5e8c250cb4eafa84c9a9e3
Harshita Kejriwal <kejriwalharshita@gmail.com>
2021-10-10 13:38:58 +0530

    UPDATE:extractingscript

--- app.py
+++ app.py
@@ -4,6 +4,18 @@ from flask import Flask, render_template, request
 import os
 # import json
 # from request import request
+import pprint
+
+import veryfi
+
+
+
+
+client_id = "vrfcnRUxjSbpO7I0ydFoNIgG5TZYXT29HThSs7G"
+client_secret = "sNAhigeiw5w28Eda7S5k3xNhdAnTn0poyhBZixsx5jAdrABi8XXANDXWv7FG9Yjruso9Yur4Ua9AC7ND4Ki0ABgb7oab2EAxYMtxCXT0PPx5vecwUPHxhASrC8T4smV6"
+username = "kejriwalharshita"
+api_key = "926fd300df925417d24f20c64871faf8"
+
 app = Flask(__name__)
 
 APP_ROOT = os.path.dirname(os.path.abspath(__file__)) # root path
@@ -20,7 +32,15 @@ def upload_func():
         print(request.files['file1'])
         f = request.files['file1']
         f.save(os.path.join(APP_ROOT, (f.filename))) # Add temp folder to path
-        print(request.method)
+        ##print(request.method)
+
+        client = veryfi.Client(client_id, client_secret, username, api_key)
+
+        categories = ["college", "professor_name", "events"]
+        json_result = client.process_document((f.filename), categories)
+
+        pprint.pprint(json_result)
+
         return 'uploaded successfully'
     return 'here!!'
 
