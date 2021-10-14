0f384cd60e1dcb3a695437f3203511cacc488b07
YASHASWI SINGH <4444yashaswi@gmail.com>
2021-10-14 14:20:13 +0530

    UPDATE: app.js DELETE submit component

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,11 @@
 import { useState } from 'react';
 import './App.css';
 import Drop from './components/Drop';
-import Submit from './components/Submit';
 
 function App() {
   return (
     <div className="App">
       <Drop/>
-      {/*<Submit/>*/}
     </div>
   );
 }
